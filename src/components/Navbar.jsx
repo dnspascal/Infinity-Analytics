391fa6d4c129016cb38d4ed63b2cfd2f08d87f15
dnspascal <dnspascal094@gmail.com>
2024-07-25 22:21:56 +0300

    Sidebar is changes width with its z-index

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,11 +24,11 @@ const Navbar = () => {
             ))}
         </ul>
         <div onClick={handleNav} className='md:hidden '>
-            {!denis ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
+            {!denis ? <AiOutlineClose size={30}/> : <AiOutlineMenu size={30}/>}
 
         </div>
         <div
-            className={!denis ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-700' : 'fixed left-[-100%]'}>
+            className={!denis ? 'fixed left-0 top-0 w-[80%] h-full border-r border-r-white/50 bg-[#000300] z-50 ease-in-out duration-700' : 'fixed left-[-100%]'}>
             <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4 italic'>INFINITY</h1>
             <ul className='uppercase p-4'>
                 {
